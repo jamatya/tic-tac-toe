077ebb467a42ced312a399bf1c4ed5178b9f5cb2
jeny_amatya@dialog.com.au <jeny_amatya@dialog.com.au>
2022-03-01 14:47:17 +1000

    Functional component

--- src/components/square.jsx
+++ src/components/square.jsx
@@ -1,18 +1,12 @@
 import React, { Component } from 'react';
-class Square extends Component {    
-   
-    render() { 
-        return (
-            <button 
-                className="square"
-                onClick={() => this.props.onClick()}
-            >
-               {
-                   this.props.value
-               }
-            </button>
-        );
-    }
+
+function Square(props){
+    return(
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    );
 }
+
  
 export default Square;
