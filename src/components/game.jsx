f871f1e3f77b717dbc8e55987341afc2f1dc7324
jeny_amatya@dialog.com.au <jeny_amatya@dialog.com.au>
2022-03-01 16:54:31 +1000

    Implementing Time Travel

--- src/components/game.jsx
+++ src/components/game.jsx
@@ -8,6 +8,7 @@ class Game extends Component {
             history: [{
                 squares: Array(9).fill(null),
             }],
+            stepNumber: 0,
             xIsNext: true,
         };
     }
@@ -26,15 +27,34 @@ class Game extends Component {
            history: history.concat([{
             squares: squares
            }]),
+           stepNumber: history.length,
             xIsNext: !this.state.xIsNext,
+        });
+    }
+
+    jumpTo(step){
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2) === 0,
         });
     }
 
        
     render() { 
         const history = this.state.history;
-        const current = history[history.length - 1];
+        const current = history[this.state.stepNumber];
         const winner = calculateWinner(current.squares);
+
+        const moves = history.map((step, move) => {
+            const desc = move ? 
+                'Go to move # ' + move :
+                'Go to game start';
+            return (
+                <li key={move}>
+                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                </li>
+            );
+        });
 
         let status;
         if(winner) {
@@ -52,7 +72,7 @@ class Game extends Component {
               </div>
               <div className="game-info">
                 <div>{ status }</div>
-                <ol>{/* TODO */}</ol>
+                <ol>{moves}</ol>
               </div>
             </div>
           );
