9471856cd9e1e2a3e6b455216aeb5ede7c519887
jeny_amatya@dialog.com.au <jeny_amatya@dialog.com.au>
2022-03-01 14:26:09 +1000

    Adding onClick event handler

--- src/components/square.jsx
+++ src/components/square.jsx
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 class Square extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }
    
     render() { 
         return (
-            <button className="square">
+            <button 
+                className="square"
+                onClick={() => this.setState({value: 'X'})}
+            >
                {
-                   this.props.value
+                   this.state.value
                }
             </button>
         );
